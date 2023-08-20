c2678138a35152968b517dbabc94f6f353da635c
Jozimar1990 <jozimar.1990@gmail.com>
2023-08-19 20:40:27 -0400

    nova atualização 19/08/2023 as 20:40

---
+++ m2a17.js
@@ -0,0 +1,10 @@
+/* 1 - Escreva um programa executando os seguintes passos:
+
+    -Crie uma variável com o valor inicial igual a uma lista de número de 1 a 5. Esse valor não deve ser alterado.
+
+    -Imprima na tela: A primeira variável. 
+
+    -Imprima na tela a seguinte mensagem: "O valor do elemento X da minha lista é $valor", substituindo o termo iniciado em $ pelo valor abrigado na sua variável. X é a posição do elemento na lista. */
+    
+    const numeros = [1 , 2 , 3 , 4 , 5];
+    console.log(numeros);
